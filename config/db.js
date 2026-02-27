664bf2350456d94ddad8032c96a9a24236516d11
Aryaeet12 <aryajeetkumar34@gmail.com>
2026-02-27 19:42:01 +0530

    update

--- config/db.js
+++ config/db.js
@@ -1,5 +1,9 @@
 import mongoose from "mongoose"
+MONGO_URI = process.MONGO_URI;
 
+if (!MONGO_URI){
+    throw new Error("MONGO_URI not found");
+}
 const dbConnect = async () => {
     await mongoose.connect(process.env.MONGO_URI);
 }
